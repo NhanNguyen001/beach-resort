c1dcfba7d468399a6ae16e60cafad61d7079a61d
Nguyen Thanh Nhan <nhan2151@gmail.com>
2020-03-06 10:57:21 +0700

    complete setup page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+
+import Home from "./pages/home";
+import Rooms from "./pages/rooms";
+import SignleRoom from "./pages/singleRoom";
+import Error from "./pages/error";
+
 import "./App.css";
 
 function App() {
-  return <>hello from app</>;
+  return (
+    <>
+      <Home />
+      <Rooms />
+      <SignleRoom />
+      <Error />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const home = () => {
+  return <div>hello from home page</div>;
+};
+
+export default home;
