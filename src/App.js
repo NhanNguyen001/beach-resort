f1af63833c37d19d62b0d917e1a842c748fe3fcb
NhanNguyen001 <nhan2151@gmail.com>
2020-03-07 13:30:45 +0700

    complete router setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import Home from "./pages/home";
-import Rooms from "./pages/rooms";
-import SignleRoom from "./pages/singleRoom";
-import Error from "./pages/error";
+import Home from './pages/home';
+import Rooms from './pages/rooms';
+import SignleRoom from './pages/singleRoom';
+import Error from './pages/error';
 
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <>
-      <Home />
-      <Rooms />
-      <SignleRoom />
-      <Error />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/rooms' component={Rooms} />
+        <Route exact path='/rooms/:slug' component={SignleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
