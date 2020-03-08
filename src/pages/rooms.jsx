fd2257163c3a499e5f8aa659ba060d4a72b04923
NhanNguyen001 <nhan2151@gmail.com>
2020-03-08 22:59:38 +0700

    complete adding banner component

--- src/pages/rooms.jsx
+++ src/pages/rooms.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Hero from '../components/hero';
+import Banner from '../components/banner';
 
 const rooms = () => {
-  return <Hero hero='roomsHero' />;
+  return (
+    <Hero hero='roomsHero'>
+      <Banner title='our rooms'>
+        <Link to='/' className='btn-primary'>
+          return Home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default rooms;
